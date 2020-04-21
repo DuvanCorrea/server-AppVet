4ff758e6690b6997e44c57dd1f67217c9cae1fc4
duvancorrea96 <duvancorrea96@gmail.com>
2020-04-20 23:06:37 -0500

    ya entiendo mejor enrutamiento :D

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const http = require("http");
+const { parse } = require("url");
 
 const server = http.createServer((req, res) => {
   //1- Obtener url del objeto req
+  const url = req.url;
+  const urlParseada = parse(url, true);
+
   //2- Obtener la ruta
+  const ruta = urlParseada.pathname;
+
   //3- enviar una respuesta dependiendo de la ruta
+  switch (ruta) {
+    case "/ruta":
+      res.end("estas en /ruta");
+      break;
+
+    default:
+      res.end("No se donde estas wey");
+      break;
+  }
 });
 
 server.listen(3000, () => {
