a63bdfe11a6fe8bc4a5b316ab1a81d4648e48639
duvancorrea96 <duvancorrea96@gmail.com>
2020-04-24 08:26:44 -0500

    crear mascota ok

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http = require("http");
 const { parse } = require("url");
-const stringDecoder = require("string_decoder").StringDecoder;
+const StringDecoder = require("string_decoder").StringDecoder;
+
+let recursos = {
+  mascotas: [
+    { tipo: "perro", nombre: "firu", propietario: "Duvan" },
+    { tipo: "gato", nombre: "michi", propietario: "Fer" },
+  ],
+};
 
 const server = http.createServer((req, res) => {
   //1- Obtener url del objeto req
@@ -9,6 +16,7 @@ const server = http.createServer((req, res) => {
 
   //2- Obtener la ruta
   const ruta = urlParseada.pathname;
+  const rutaLimpia = ruta.replace("/", "");
 
   //2.1- Obtener method
   const method = req.method.toLowerCase();
@@ -20,22 +28,67 @@ const server = http.createServer((req, res) => {
   const { headers = {} } = req;
 
   //2.4- Obtener payload, bueno si hay uno
-  const decoder = new StringDecoder("utf-8");
-  const buffer = "";
+  let decoder = new StringDecoder("utf-8");
+  let buffer = "";
 
-  req.on("data", () => {});
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
 
-  //3- enviar una respuesta dependiendo de la ruta
-  switch (ruta) {
-    case "/ruta":
-      res.end("estas en /ruta");
-      break;
+  req.on("end", (data) => {
+    buffer += decoder.end();
 
-    default:
-      res.end("No se donde estas wey");
-      break;
-  }
+    // convertir bufer en json
+    if (headers["content-type"] === "application/json") {
+      buffer = JSON.parse(buffer);
+    }
+
+    //2.5 ordenar los datos (data)
+    const dataOrganizada = {
+      ruta: rutaLimpia,
+      query: query,
+      method: method,
+      headers: headers,
+      payload: buffer,
+    };
+
+    // 2.6 manejador de respuesta HANDLER
+    let handler;
+    if (rutaLimpia && enrutador[rutaLimpia][method]) {
+      handler = enrutador[rutaLimpia][method];
+    } else {
+      handler = enrutador.noEncontrado;
+    }
+
+    //4- enviar una respuesta dependiendo de la ruta
+    if (typeof handler === "function") {
+      handler(dataOrganizada, (statusCode = 200, mensaje) => {
+        const respuesta = JSON.stringify(mensaje);
+        res.writeHead(statusCode);
+        //respuesta real
+        res.end(respuesta);
+      });
+    }
+  });
 });
+
+const enrutador = {
+  ruta: (data, callback) => {
+    callback(200, { mensaje: "Esta es ruta" });
+  },
+  mascotas: {
+    get: (data, callback) => {
+      callback(200, recursos.mascotas);
+    },
+    post: (data, callback) => {
+      recursos.mascotas.push(data.payload);
+      callback(201, data.payload);
+    },
+  },
+  noEncontrado: (data, callback) => {
+    callback(404, { mensaje: "Error 404" });
+  },
+};
 
 server.listen(3000, () => {
   console.warn("Corriendo en el pueto 3000");
